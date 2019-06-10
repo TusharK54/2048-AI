33a93f799305bf13ff474af6f4426641598921e8
Tushar Khan <tushar.54k@gmail.com>
2019-06-10 11:09:38 -0400

    Changed file names

---
+++ 2048-AI/color_constants.py
@@ -0,0 +1,27 @@
+
+INFO_BACKGROUND         = '#faf8ef'
+TITLE                   = '#453C33'
+SCORE_BOX               = '#bbada0'
+SCORE_TITLE             = '#eee4da'
+DEFAULT_SIZE_BUTTON     = '#f59563'
+ENABLED_SIZE_BUTTON     = '#f65e3b'
+DEFAULT_STANDARD_BUTTON = '#8f7a66'
+ENABLED_STANDARD_BUTTON = 'brown'
+BUTTON_LABELS           = '#f9f6f2'
+TILE_GRID               = '#938578'
+GAME_OVER_STIPPLE       = '#eee4da'
+
+TILES = {}
+TILES['default']    = {'tile': '#3c3a32', 'text': '#f9f6f2'}
+TILES[str(0)]       = {'tile': '#bbada0', 'text': '#bbada0'}
+TILES[str(2)]       = {'tile': '#eee4da', 'text': '#453C33'}
+TILES[str(4)]       = {'tile': '#ede0c8', 'text': '#453C33'}
+TILES[str(8)]       = {'tile': '#f2b179', 'text': '#f9f6f2'}
+TILES[str(16)]      = {'tile': '#f59563', 'text': '#f9f6f2'}
+TILES[str(32)]      = {'tile': '#f67c5f', 'text': '#f9f6f2'}
+TILES[str(64)]      = {'tile': '#f65e3b', 'text': '#f9f6f2'}
+TILES[str(128)]     = {'tile': '#edcf72', 'text': '#f9f6f2'}
+TILES[str(256)]     = {'tile': '#edcc61', 'text': '#f9f6f2'}
+TILES[str(512)]     = {'tile': '#edc850', 'text': '#f9f6f2'}
+TILES[str(1024)]    = {'tile': '#edc53f', 'text': '#f9f6f2'}
+TILES[str(2048)]    = {'tile': '#edc22e', 'text': '#f9f6f2'}
